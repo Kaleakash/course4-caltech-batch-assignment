663d794831aac2af7affa0997af1de19427b7ec2
akash <akash300383@gmail.com>
2024-03-12 09:07:28 +0530

    done

--- src/main/java/com/controller/SimpleController.java
+++ src/main/java/com/controller/SimpleController.java
@@ -8,7 +8,10 @@ public class SimpleController {
 	
 	@GetMapping(value = "/")
 	public String sayHello() {
-		String msg=  "<div style='color:red;background-color:yellow;align:center;'>Welcome DrShawn pet clinic. This appliatication develop using spring boot</div>";
+		String msg=  "<div style='color:red;background-color:yellow;font-size:40pt;'>"
+				+ "Welcome DrShawn pet clinic. This appliatication develop using spring boot"
+				+ "We are developing application using Spring, EC2, Docker, Jenkin etc"
+				+ "</div>";
 		return msg;
 	}
 }
